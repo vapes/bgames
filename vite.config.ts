af1485b6fcd462458234ff902e58824e4f56ed7e
Valery Piasetski <valery.piasetski@bgaming.com>
2025-08-08 18:31:58 +0200

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,14 @@ export default defineConfig({
 	},
 	build: {
 		outDir: 'dist',
-		sourcemap: true
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				assetFileNames: 'assets/[name]-[hash][extname]',
+				chunkFileNames: 'assets/[name]-[hash].js',
+				entryFileNames: 'assets/[name]-[hash].js'
+			}
+		}
 	},
 	base: './' // Важно для GitHub Pages
 });
